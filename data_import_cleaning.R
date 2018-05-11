a8557829967831ca6f789ae52d12627054b3e4d9
nagyrc <nagyrc@gmail.com>
2018-05-11 11:11:37 -0600

    clean Jones data

--- data_import_cleaning.R
+++ data_import_cleaning.R
@@ -9,13 +9,12 @@ setwd("data/")
 
 file_names <- list.files()
 
+#Jones data# does not currently include the pre and post burn data (Jones_burn.csv)
 Jones_soil <- as.data.frame(read_csv("Jones_soil.csv"))
-Jones_burn <- as.data.frame(read_csv("Jones_burn.csv"))
 Jones_veg_only <- as.data.frame(read_csv("Jones_veg_only.csv"))
 Jones_litter_only <- as.data.frame(read_csv("Jones_litter_only.csv"))
 
 head(Jones_soil)
-head(Jones_burn)
 head(Jones_veg_only)
 head(Jones_litter_only)
 
@@ -23,10 +22,12 @@ head(Jones_litter_only)
 Jones_veg_litter <- merge(Jones_veg_only, Jones_litter_only, 
                           by = c("Barrel","Site","Litter_trt","Burn_trt","Rep","Year"))
 
-head(Jones_veg_litter)
-
 #merge veg_litter and soil dataframes
-Jones_veg_litter_soil <- merge(Jones_veg_litter, Jones_soil, 
+Jones_vls <- merge(Jones_veg_litter, Jones_soil, 
                           by = c("Barrel","Site","Litter_trt","Burn_trt","Rep","Year"))
 
-head(Jones_veg_litter_soil)
+#add fields that will be common across studies
+Jones_vls$burned <- ifelse(Jones_vls$Burn_trt == 'B' | Jones_vls$Burn_trt == 'C', 'yes','no')
+Jones_vls$study <- "Jones et al. 2015"
+
+
