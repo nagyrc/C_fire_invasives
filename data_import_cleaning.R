aee84640a7c998a10e2b990e232bf372c99aba8f
nagyrc <nagyrc@gmail.com>
2018-05-11 10:07:10 -0600

    opened Jones files for cleaning

--- data_import_cleaning.R
+++ data_import_cleaning.R
@@ -4,3 +4,13 @@
 
 library(tidyverse)
 library(plyr)
+
+setwd("data/")
+
+file_names <- list.files()
+
+Jones_soil <- as.data.frame(read_csv("Jones_soil.csv"))
+Jones_veg <- as.data.frame(read_csv("Jones_veg.csv"))
+
+head(Jones_soil)
+head(Jones_veg)
