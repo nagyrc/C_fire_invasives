b08ae7d3b1a38de5844f20cb447176031abee08e
nagyrc <nagyrc@gmail.com>
2018-05-11 10:32:58 -0600

    merging Jones data

--- data_import_cleaning.R
+++ data_import_cleaning.R
@@ -10,7 +10,17 @@ setwd("data/")
 file_names <- list.files()
 
 Jones_soil <- as.data.frame(read_csv("Jones_soil.csv"))
-Jones_veg <- as.data.frame(read_csv("Jones_veg.csv"))
+Jones_burn <- as.data.frame(read_csv("Jones_veg_litter.csv"))
+Jones_veg_only <- as.data.frame(read_csv("Jones_veg_only.csv"))
+Jones_litter_only <- as.data.frame(read_csv("Jones_litter_only.csv"))
 
 head(Jones_soil)
-head(Jones_veg)
+head(Jones_burn)
+head(Jones_veg_only)
+head(Jones_litter_only)
+
+#merge veg and litter dataframes
+Jones_veg_litter <- merge(Jones_veg_only, Jones_litter_only, 
+                          by = c("Barrel","Site","Litter_Trt","Burn_Trt","Rep","Year"))
+
+rm(Jones_veg)
