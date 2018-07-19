5fad4d85a377c48cfae5cc064093c4b8599a7c90
nagyrc <nagyrc@gmail.com>
2018-07-19 15:11:54 -0600

    checking the data

--- data_exploration.R
+++ data_exploration.R
@@ -17,18 +17,19 @@ alldata <- as.data.frame(read_csv("alldata.csv"))
 #data exploration
 summary(alldata$AGBC_g_m2, na.rm = TRUE)
 #why is there a zero???
-#695 NAs
+#755 NAs
 
 zeroAGB <- alldata[alldata$AGBC_g_m2 == 0, ]
-#700, so 695 NAs and 5 zeros
+#760, so 755 NAs and 5 zeros
 
 ord <- alldata[order(alldata$AGBC_g_m2),] 
 head(ord)
 #the zeros are from Davies et al. 2009 that had zero cheatgrass biomass
 
 summary(alldata$soilC_g_m2)
-#494 NAs
+#496 NAs
 #need to check the units! huge variation
+#some of this could be due to different depths
 
 #need to remove NAs...tried na.rm, na.omit, na.exclude
 hist(alldata$soilC_g_m2, breaks = 20)
@@ -49,4 +50,10 @@ qplot(alldata$soilC_g_m2, geom = "histogram", color = alldata$study)
 ggplot(alldata, aes(x = yr_samp, y = soilC_g_m2, color = study)) + geom_point()
 
 #
-summaryBy(soilC_g_m2 ~ study, data=alldata)
+sum1 <- summaryBy(soilC_g_m2 ~ study, data = alldata, FUN = mean)
+sum2 <- summaryBy(bottomdepth_cm ~ study, data = alldata, FUN = max)
+
+sumjoin <- left_join(sum1, sum2, by = "study")
+sumjoin
+#check depths on Norton et al. 2004: 99 cm?
+#check depths on Rau et al. 2011: no bottom depth listed
