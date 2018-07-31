c02830c95a609d90542bdf63694d71a1de421639
nagyrc <nagyrc@gmail.com>
2018-07-31 14:09:19 -0600

    creating attribute table

--- data_ninja.R
+++ data_ninja.R
@@ -20,4 +20,13 @@ head(studyid)
 artstud <- unique(studyid[c("Article_ID", "Study_ID")])
 
 write.csv(artstud, file = "/Users/rana7082-su/Dropbox/C_fire_invasives_R/results/articlestudyid.csv")
+is.numeric(studyid$topdepth_cm)
+is.numeric(studyid$bottomdepth_cm)
 
+#recalculate thickness; there were some issues with this when we looked at alldatall.csv
+studyid$thick <- studyid$bottomdepth_cm - studyid$topdepth_cm
+#this fixed it
+
+
+att1 <- unique(studyid[c("Article_ID", "Study_ID", "veg", "topdepth_cm", "bottomdepth_cm", "BD_estimated","yr_samp", "lat","long")])
+write.csv(att1, file = "/Users/rana7082-su/Dropbox/C_fire_invasives_R/results/attributes1.csv")
