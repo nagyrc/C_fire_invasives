61882a387100e3fcad3923f68a37223938420bc2
nagyrc <nagyrc@gmail.com>
2018-07-31 16:27:54 -0600

    moving to new R script

--- data_ninja.R
+++ data_ninja.R
@@ -30,3 +30,29 @@ studyid$thick <- studyid$bottomdepth_cm - studyid$topdepth_cm
 
 att1 <- unique(studyid[c("Article_ID", "Study_ID", "veg", "topdepth_cm", "bottomdepth_cm", "BD_estimated","yr_samp", "lat","long")])
 write.csv(att1, file = "/Users/rana7082-su/Dropbox/C_fire_invasives_R/results/attributes1.csv")
+
+
+#att2 <- unique(studyid[c("Article_ID", "Study_ID", "soilC_g_m2", "BGBC_g_m2", "litterC_g_m2", "AGBC_g_m2")])
+#head(att2)
+
+sum71 <- summaryBy(litterC_g_m2 ~ Article_ID + Study_ID, data = studyid)
+sum72 <- summaryBy(soilC_g_m2 ~ Article_ID + Study_ID, data = studyid)
+sum73 <- summaryBy(BGBC_g_m2 ~ Article_ID + Study_ID, data = studyid)
+sum74 <- summaryBy(AGBC_g_m2 ~ Article_ID + Study_ID, data = studyid)
+
+combo1 <- left_join(sum71, sum72, by = c("Article_ID","Study_ID"))
+combo2 <- left_join(combo1, sum73, by = c("Article_ID","Study_ID"))
+combo3 <- left_join(combo2, sum74, by = c("Article_ID","Study_ID"))
+
+head(combo3)
+tail(combo3)
+
+#replace NAs with negative number
+combo3[is.na(combo3)] <- -2
+
+combo3$Carbon_pool <- ifelse(combo3$soilC_g_m2.mean > 0, "soil",
+                              ifelse(combo3$litterC_g_m2.mean > 0, "litter",
+                                     ifelse(combo3$AGBC_g_m2.mean > 0, "AGB", "BGB")))
+
+
+write.csv(combo3, file = "/Users/rana7082-su/Dropbox/C_fire_invasives_R/results/attributes2.csv")
