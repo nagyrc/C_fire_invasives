8212c67b4a0c5111ad814c87f5c260a7d8acb83a
nagyrc <nagyrc@gmail.com>
2018-11-30 09:38:40 -0700

    updating results with new alldata.csv

--- 1b_mean_BD_calcs.R
+++ 1b_mean_BD_calcs.R
@@ -8,18 +8,18 @@ alldata <- as.data.frame(read_csv("alldata.csv"))
 
 #find mean BD from studies that have BD data
 #0-10 cm
-sub1 <- alldata[ which(alldata$topdepth_cm == 0 & alldata$bottomdepth_cm == 10),]
+sub1 <- alldata[ which(alldata$topdepth_cm == 0 & alldata$bottomdepth_cm == 10 & alldata$BD_estimated == "no"),]
 
 summary(sub1$BD_g_cm3)
-#mean = 1.417; based on 95 observations across studies
+#mean = 1.422; based on 95 observations across studies
 
 #10-20 cm
-sub2 <- alldata[ which(alldata$topdepth_cm == 10 & alldata$bottomdepth_cm == 20),]
+sub2 <- alldata[ which(alldata$topdepth_cm == 10 & alldata$bottomdepth_cm == 20 & alldata$BD_estimated == "no"),]
 
 summary(sub2$BD_g_cm3)
 #mean = 1.35; based on 8 observations from Stark BD data
 
-sub3 <- alldata[ which(alldata$bottomdepth_cm == 10 | alldata$bottomdepth_cm == 20),]
+sub3 <- alldata[ which(alldata$bottomdepth_cm == 0 | alldata$bottomdepth_cm == 20 & alldata$BD_estimated == "no"),]
 
 summary(sub3$BD_g_cm3)
 #1.408
