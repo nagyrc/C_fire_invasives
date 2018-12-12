dce195feb2d776a2d685b5b81adf8fc95b8473ed
nagyrc <nagyrc@gmail.com>
2018-12-12 11:21:47 -0700

    adding total vs. organic

--- 3_data_prep_for_metaanalysis.R
+++ 3_data_prep_for_metaanalysis.R
@@ -14,13 +14,15 @@ colnames(alldata)[colnames(alldata) == 'orgsoil%C'] <- 'orgsoilperC'
 colnames(alldata)[colnames(alldata) == 'totsoil%C'] <- 'totsoilperC'
 
 #recalculate thickness- we saw some issues with it in the .csv file
-is.numeric(alldata$topdepth_cm)
-is.numeric(alldata$bottomdepth_cm)
-alldata$thick <- alldata$bottomdepth_cm - alldata$topdepth_cm
+#is.numeric(alldata$topdepth_cm)
+#is.numeric(alldata$bottomdepth_cm)
+
+#alldata$check <- alldata$bottomdepth_cm - alldata$topdepth_cm
+#identical(alldata$check, alldata$thick)
 
 #slims dataframe to key variables...still wide format to use with spatial data in script 4 (adding fire data)
 clean_study <- alldata %>%
-  dplyr::select("site","yr_samp","AGBC_g_m2","BGBC_g_m2","litterC_g_m2","soilperC","BD_g_cm3","soilC_g_m2","topdepth_cm","bottomdepth_cm","BD_estimated","veg","study","lat","long","thick") %>%
+  dplyr::select("site","yr_samp","AGBC_g_m2","BGBC_g_m2","litterC_g_m2","orgsoilperC", "totsoilperC", "BD_g_cm3","orgsoilC_g_m2","totsoilC_g_m2","topdepth_cm","bottomdepth_cm","BD_estimated","veg","study","lat","long","thick") %>%
   mutate(site = as.factor(site),
          veg = as.factor(veg)) %>%
   mutate(yr_samp = as.numeric(ifelse(is.na(yr_samp), 0, yr_samp))) %>%
@@ -40,6 +42,8 @@ alldata$Article_ID <- paste(toupper(alldata$Article_IDs),alldata$Article_IDe)
 alldata$Article_ID <- gsub(" ", "", alldata$Article_ID) 
 alldata$Article_ID <- gsub("MAHOpub1", "MAHO2018a", alldata$Article_ID) 
 alldata$Article_ID <- gsub("MAHOpub2", "MAHO2018b", alldata$Article_ID) 
+alldata$Article_ID <- gsub("RICK985a", "RICK1985b", alldata$Article_ID)
+alldata$Article_ID <- gsub("RICK985b", "RICK1985b", alldata$Article_ID)
 
 unique(alldata$Article_ID)
 #these look great
@@ -47,10 +51,10 @@ unique(alldata$Article_ID)
 
 #creates study_ID, pool, and setup for Bethany's meta-analysis format (long format)
 studyid <- alldata %>%
-  dplyr::select("site","yr_samp","AGBC_g_m2","BGBC_g_m2","litterC_g_m2","soilperC","BD_g_cm3","soilC_g_m2","topdepth_cm","bottomdepth_cm","BD_estimated","veg","study","lat","long","thick","Article_ID") %>%
-  tidyr::gather(key = variable, value = value, -site, -study, -yr_samp, -lat, -long, -veg, -thick, -BD_estimated, -topdepth_cm, -bottomdepth_cm, -soilperC, -BD_g_cm3, -Article_ID) %>%
+  dplyr::select("site","yr_samp","AGBC_g_m2","BGBC_g_m2","litterC_g_m2","totsoilperC","orgsoilperC","BD_g_cm3","totsoilC_g_m2","orgsoilC_g_m2","topdepth_cm","bottomdepth_cm","BD_estimated","veg","study","lat","long","thick","Article_ID") %>%
+  tidyr::gather(key = variable, value = value, -site, -study, -yr_samp, -lat, -long, -veg, -thick, -BD_estimated, -topdepth_cm, -bottomdepth_cm, -totsoilperC, -orgsoilperC, -BD_g_cm3, -Article_ID) %>%
   mutate(variable = as.factor(variable),
-         pool = ifelse(AGBC_g_m2 > 0, "AGB", ifelse(BGBC_g_m2 > 0, "BGB", ifelse(litterC_g_m2 > 0, "litter", "soil"))), 
+         pool = ifelse(AGBC_g_m2 > 0, "AGB", ifelse(BGBC_g_m2 > 0, "BGB", ifelse(litterC_g_m2 > 0, "litter", ifelse(totsoilC_g_m2 > 0, "total soil","organic soil")))), 
          Study_ID = group_indices_(., .dots = c("study","lat", "long", "veg", "site", "bottomdepth_cm", "pool","yr_samp"))) %>%
 #check nested ifelse statements to make sure it is creating pool
 
