caab8536f6afcd74ca9d90068fadd3ffbee6dcc9
nagyrc <nagyrc@gmail.com>
2019-02-04 14:22:17 -0700

    cleaning code, but it's still not working

--- 4_add_fire_data.R
+++ 4_add_fire_data.R
@@ -3,14 +3,14 @@
 #created August 30, 2018
 
 #load multiple libraries 
-x <- c("tidyverse", "sf", "assertthat", "purrr", "httr", "plyr", "stringr", "raster", "ggplot2", "doBy", "reshape", "velox")
+x <- c("tidyverse", "sf", "assertthat", "purrr", "httr", "plyr", "stringr", "raster", "ggplot2", "doBy", "reshape", "velox", "sp")
 lapply(x, library, character.only = TRUE, verbose = FALSE)
 
 setwd("data/")
 getwd()
 
-#set crs for all data layers: Albers equal area
-crs <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
+#set crs for all data layers: epsg projection 4326 - wgs 84
+crs1 <- 4326
 
 #Download US shapefile
 us_shp <- file.path('states_shp', "cb_2016_us_state_20m.shp")
@@ -30,14 +30,16 @@ if (!file.exists(us_shp)) {
 }
 
 
-#bring in shapefile of US states; select 48 contiguous; tranform to match crs of other layers
+#bring in shapefile of US states; select 48 contiguous; tranform to match crs of other layers; remove extra fields
 usa_shp <- st_read(file.path('states_shp'), layer = 'cb_2016_us_state_20m') %>%
   filter(!(NAME %in% c("Alaska", "Hawaii", "Puerto Rico"))) %>%
-  st_transform(st_crs(crs)) %>%  # set to Albers equal area
   dplyr::select(STATEFP, STUSPS) %>%
-  setNames(tolower(names(.)))
+  setNames(tolower(names(.))) %>% 
+  st_transform(crs1)
 
+class(usa_shp)
 
+crs(usa_shp)
 ###########################
 #MTBS
 #Download the MTBS fire polygons
@@ -55,15 +57,15 @@ if (!file.exists(mtbs_shp)) {
 #bring in MTBS data
 mtbs_fire <- st_read(dsn = 'mtbs',
                      layer = "mtbs_perims_DD", quiet = TRUE) %>%
-  st_transform(st_crs(crs)) %>%
   mutate(MTBS_ID = Fire_ID,
          MTBS_DISCOVERY_YEAR = Year) %>%
-  dplyr::select(MTBS_ID, MTBS_DISCOVERY_YEAR)
+  dplyr::select(MTBS_ID, MTBS_DISCOVERY_YEAR) %>%
+  st_transform(., crs1)
 
 
-#bring in dataframe and convert dataframe to sf object with Albers equal area projection
+#bring in dataframe and convert dataframe to sf object with epsg projection 4326 - wgs 84
 studyid = read_csv("studyid.csv")
-studyid_sf  <-  st_as_sf(studyid, coords = c('long', 'lat'), crs = crs)
+studyid_sf  <-  st_as_sf(studyid, coords = c('long', 'lat'), crs = crs1)
 
 #make sure they all match
 crs(studyid_sf)
@@ -73,15 +75,15 @@ crs(mtbs_fire)
 
 #extract discovery year for points in studyid
 mtbs_test <- mtbs_fire  %>%
-  sf::st_intersection(., studyid_sf) %>%
+  sf::st_intersection(., studyid) %>%
   mutate(mtbs_keep = ifelse(MTBS_DISCOVERY_YEAR <= yr_samp, 1, 0)) %>%
   filter(mtbs_keep != 0) %>%
   dplyr::select(-mtbs_keep) %>%
   group_by(id) %>%
   summarise(last_burn_year = max(MTBS_DISCOVERY_YEAR))
 
-#adding MTBS last year burn to clean_study
-mtbs_clean <- clean_study %>%
+#adding MTBS last year burn to studyid_df
+mtbs_clean <- studyid_sf %>%
   left_join(., as.data.frame(mtbs_test) %>% dplyr::select(-geometry), by = 'id')
 
 
@@ -124,33 +126,30 @@ layer_reclass <- function(dir) {
 
 yearly_modis <- layer_reclass(dir = dir)
 
-library(velox)
 
-#transform dataframe into equal area projection
-clean_study_laea <- clean_study %>%
-  st_transform('+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs')
+
 
 ###option #1
-#extract modis at points in clean_study
-modis_df <- velox(yearly_modis)$extract_points(sp = clean_study_laea) %>%
+#extract modis at points in studyid
+modis_df <- velox(yearly_modis)$extract_points(sp = studyid_sf) %>%
   as_tibble()
 colnames(modis_df) <- names(yearly_modis)
 
-#create last year burned at points in clean_study
+#create last year burned at points in studyid
 modis_df2 <- modis_df   %>%
-  mutate(id = as.data.frame(clean_study)$id) %>%
+  mutate(id = as.data.frame(studyid_sf)$id) %>%
   gather(key = key, value = last_burn_year_modis , -id) %>%
   dplyr::select(-key) %>%
   filter(last_burn_year_modis != 0)
 ###
 
 ###option #2
-modis_df <- velox(yearly_modis)$extract(sp = clean_study_laea, fun = function(x) max(x, na.rm=TRUE), small = TRUE, df = TRUE) %>%
+modis_df <- velox(yearly_modis)$extract(sp = studyid_sf, fun = function(x) max(x, na.rm=TRUE), small = TRUE, df = TRUE) %>%
   as_tibble()
 colnames(modis_df) <- c('ID_sp', names(yearly_modis))
 
 modis_df2 <- modis_df   %>%
-  mutate(id = as.data.frame(clean_study)$id) %>%
+  mutate(id = as.data.frame(studyid_sf)$id) %>%
   dplyr::select(-ID_sp) %>%
   gather(key = key, value = last_burn_year_modis , -id) %>%
   dplyr::select(-key) %>%
@@ -161,15 +160,13 @@ modis_df2 <- modis_df   %>%
 ###########################
 #BAECV
 #bring in BAECV last year burned from Adam
-library(raster)
-
 baecvlyb <- raster("baecv/lyb_usa_baecv_1984_2015.tif")
 
-str(clean_study)
+str(studyid_sf)
 
 #convert to shapefile
 
-#then clean_study$baecvlyb <- raster::extract (baecvlyb, clean_study)
+#then studyid$baecvlyb <- raster::extract (baecvlyb, studyid)
 
 #find points where yr_samp < lyb, then need time -1
 #give Adam a shapefile of these points 
