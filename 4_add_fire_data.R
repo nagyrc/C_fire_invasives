41f5f13943a0e442a886acf905e0bf609732da2b
nagyrc <nagyrc@gmail.com>
2019-02-04 11:46:48 -0700

    notes

--- 4_add_fire_data.R
+++ 4_add_fire_data.R
@@ -9,7 +9,7 @@ lapply(x, library, character.only = TRUE, verbose = FALSE)
 setwd("data/")
 getwd()
 
-#set crs for all data layers
+#set crs for all data layers: Albers equal area
 crs <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
 
 #Download US shapefile
