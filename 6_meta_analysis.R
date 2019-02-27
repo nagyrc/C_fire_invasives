aba0255236300a0e1e7918133a400fa7a77446ba
nagyrc <nagyrc@gmail.com>
2019-02-27 15:15:21 -0700

    updating meta-analysis code

--- 6_meta_analysis.R
+++ 6_meta_analysis.R
@@ -10,10 +10,10 @@ setwd("data/")
 
 
 ## get full dataset from github (includes all cases and studies)
-d1 <- read.csv("Meta_analysis_sheet_manual.csv", header = T)
-head(d1)
-summary(d1) # no missing values in n, mean, variance
-dim(d1) #39 x 14
+dq2 <- read.csv("Meta_analysis_sheet_manual.csv", header = T)
+head(dq2)
+summary(dq2) # no missing values in n, mean, variance
+
 
 
 ################################################
@@ -25,29 +25,46 @@ dim(d1) #39 x 14
 # Computing observation-level SD
 #converting se and ci into sd
 #for us "control" will probably be the sagecheat sites
-dq2$SD_control[dq2$var_statistic == "SE"] <- dq2$var_control[dq2$var_statistic == "SE"]*sqrt(dq2$n_control[dq2$var_statistic == "SE"])
-dq2$SD_control[dq2$var_statistic == "CI"] <- (sqrt(dq2$n_control[dq2$var_statistic == "CI"]))*(((dq2$mean_control[dq2$var_statistic == "CI"] + dq2$var_control[dq2$var_statistic == "CI"])- dq2$mean_control[dq2$var_statistic == "CI"])/1.96)
+#Not necessary, we already have SD calculated
+
+#dq2$SD_control[dq2$var_statistic == "SE"] <- dq2$varsage[dq2$var_statistic == "SE"]*sqrt(dq2$nsage[dq2$var_statistic == "SE"])
+#dq2$SD_control[dq2$var_statistic == "CI"] <- (sqrt(dq2$nsage[dq2$var_statistic == "CI"]))*(((dq2$meansage[dq2$var_statistic == "CI"] + dq2$varsage[dq2$var_statistic == "CI"])- dq2$meansage[dq2$var_statistic == "CI"])/1.96)
 
 #will need to calculate sd for the invaded and invaded fire sites as well
 
 # Computing the pooled standard deviations for the Hedges' index
 #this is the equation in the book
-#inv is the invasion treatment
-dq2$SD_pool_inv <- sqrt((dq2$SD_control^2*(dq2$n_control-1)+dq2$SD_inv^2*(dq2$n_inv-1))/(dq2$n_control+dq2$n_inv-2))
+#at this point we have three groups - cheat (invaded, burned), sagecheat (invaded, unburned), and sage (uninvaded, unburned)
+dq2$SD_hedge_cheat_v_sage <- sqrt((dq2$sdsage^2*(dq2$nsage-1)+dq2$sdcheat^2*(dq2$ncheat-1))/(dq2$nsage+dq2$ncheat-2))
+dq2$SD_hedge_cheat_v_sagecheat <- sqrt((dq2$sdsagecheat^2*(dq2$nsagecheat-1)+dq2$sdcheat^2*(dq2$ncheat-1))/(dq2$nsagecheat+dq2$ncheat-2))
+dq2$SD_hedge_sagecheat_v_sage <- sqrt((dq2$sdsage^2*(dq2$nsage-1)+dq2$sdsagecheat^2*(dq2$nsagecheat-1))/(dq2$nsage+dq2$nsagecheat-2))
+
 
 ## Compute J (an adjustment for small n)
 #J is also in the book- best for studies with small sample sizes
 #calculate one per effect size- here this is control vs invasion
-dq2$df_inv = dq2$n_control + dq2$n_inv - 2
-dq2$J_inv = 1 - (3 / (4*dq2$df_inv -1))
+dq2$df_cheat_v_sage = dq2$nsage + dq2$ncheat - 2
+dq2$J_cheat_v_sage = 1 - (3 / (4*dq2$df_cheat_v_sage -1))
+
+dq2$df_cheat_v_sagecheat = dq2$nsagecheat + dq2$ncheat - 2
+dq2$J_cheat_v_sagecheat = 1 - (3 / (4*dq2$df_cheat_v_sagecheat -1))
+
+dq2$df_sagecheat_v_sage = dq2$nsage + dq2$nsagecheat - 2
+dq2$J_sagecheat_v_sage = 1 - (3 / (4*dq2$df_sagecheat_v_sage -1))
 
 ## Compute effect sizes (Hedge's g) with the J adjustment
 #result is the effect size for each row in the data set 
-dq2$gInv <- ((dq2$mean_inv - dq2$mean_control)/ dq2$SD_pool_inv)*dq2$J_inv
+dq2$g_cheat_v_sage <- ((dq2$meancheat - dq2$meansage)/ dq2$SD_hedge_cheat_v_sage)*dq2$J_cheat_v_sage
+dq2$g_cheat_v_sagecheat <- ((dq2$meancheat - dq2$meansagecheat)/ dq2$SD_hedge_cheat_v_sagecheat)*dq2$J_cheat_v_sagecheat
+dq2$g_sagecheat_v_sage <- ((dq2$meansagecheat - dq2$meansage)/ dq2$SD_hedge_sagecheat_v_sage)*dq2$J_sagecheat_v_sage
+
 
 ## Calculate Variance (MEV)-- Whitlock 2014
 #calculates the variance of hedges statistic
-dq2$var_d_inv = ((dq2$n_control + dq2$n_inv/dq2$n_control * dq2$n_inv) + dq2$gInv^2/(2*(dq2$n_control + dq2$n_inv -2))) * ((dq2$n_control + dq2$n_inv)/(dq2$n_control + dq2$n_inv -2))
+dq2$var_d_cheat_v_sage = ((dq2$nsage + dq2$ncheat/dq2$nsage * dq2$ncheat) + dq2$g_cheat_v_sage^2/(2*(dq2$nsage + dq2$ncheat -2))) * ((dq2$nsage + dq2$ncheat)/(dq2$nsage + dq2$ncheat -2))
+dq2$var_d_cheat_v_sagecheat = ((dq2$nsagecheat + dq2$ncheat/dq2$nsagecheat * dq2$ncheat) + dq2$g_cheat_v_sagecheat^2/(2*(dq2$nsagecheat + dq2$ncheat -2))) * ((dq2$nsagecheat + dq2$ncheat)/(dq2$nsagecheat + dq2$ncheat -2))
+dq2$var_d_sagecheat_v_sage = ((dq2$nsage + dq2$nsagecheat/dq2$nsage * dq2$nsagecheat) + dq2$g_sagecheat_v_sage^2/(2*(dq2$nsage + dq2$nsagecheat -2))) * ((dq2$nsage + dq2$nsagecheat)/(dq2$nsage + dq2$nsagecheat -2))
+
 
 ####################################################################
 ## Meta-Analysis
@@ -56,6 +73,8 @@ library(MCMCglmm)
 library(MCMCvis)
 
 #subset data by carbon pool
+orgsoil <- subset(dq2,pool=="orgsoilC_g_m2"&!is.na(var_d_cheat_v_sage))
+orgsoil2 <- subset(dq2,pool=="orgsoilC_g_m2"&!is.na(var_d_sagecheat_v_sage))
 
 # set priors
 #non informative uniform priors
@@ -65,20 +84,25 @@ prior <- list(R=list(V = 1e-10, nu = -1), G = list(G1 = list(V = 1e-10, nu = -1)
 # we will get an error because we don't have replication with our study ID. this is OK
 #make nitt smaller- 10,000 when we start to help run faster
 #fit= dq2
-m1a_inv <- MCMCglmm(gInv ~ 1, random = ~studyID, mev = fit$var_d_inv,
-                    prior = prior, nitt = 1000000, burnin = 10000, thin = 100, verbose = T,
-                    data = fit, pr = T, saveX = T, saveZ = T)
+m1a_inv <- MCMCglmm(g_cheat_v_sage ~ 1, random = ~Article_ID, mev = orgsoil$var_d_cheat_v_sage,
+                    prior = prior, nitt = 100000, burnin = 10000, thin = 100, verbose = T,
+                    data = orgsoil, pr = T, saveX = T, saveZ = T)
 
-m1b_inv <- MCMCglmm(gInv ~ 1, random = ~studyID, mev = fit$var_d_inv,
-                    prior = prior, nitt = 1000000, burnin = 10000, thin = 100, verbose = T,
-                    data = fit, pr = T, saveX = T, saveZ = T)
+m1b_inv <- MCMCglmm(g_cheat_v_sage ~ 1, random = ~Article_ID, mev = orgsoil$var_d_cheat_v_sage,
+                    prior = prior, nitt = 100000, burnin = 10000, thin = 100, verbose = T,
+                    data = orgsoil, pr = T, saveX = T, saveZ = T)
 
-m1c_inv <- MCMCglmm(gInv ~ 1, random = ~studyID, mev = fit$var_d_inv,
-                    prior = prior, nitt = 1000000, burnin = 10000, thin = 100, verbose = T,
-                    data = fit, pr = T, saveX = T, saveZ = T)
+m1b_inv <- MCMCglmm(g_cheat_v_sage ~ 1, random = ~Article_ID, mev = orgsoil$var_d_cheat_v_sage,
+                    prior = prior, nitt = 100000, burnin = 10000, thin = 100, verbose = T,
+                    data = orgsoil, pr = T, saveX = T, saveZ = T)
 summary(m1a_inv)
 summary(m1b_inv)
 summary(m1c_inv)
+
+m2a_inv <- MCMCglmm(g_sagecheat_v_sage ~ 1, random = ~Article_ID, mev = orgsoil2$var_d_sagecheat_v_sage,
+                    prior = prior, nitt = 100000, burnin = 10000, thin = 1000, verbose = T,
+                    data = orgsoil2, pr = T, saveX = T, saveZ = T)
+
 
 #NOTE that if we want to include a fixed effect then gInv~ would have a variable listed
 
