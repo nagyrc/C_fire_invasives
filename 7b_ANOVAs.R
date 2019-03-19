e7e954a4838cdd25984cb6281886a007a913f244
nagyrc <nagyrc@gmail.com>
2019-03-19 12:57:58 -0600

    checking assumptions and running non-parametric alternatives

--- 7b_ANOVAs.R
+++ 7b_ANOVAs.R
@@ -27,8 +27,9 @@ orgsoil1020 <- orgsoilC2 %>%
 totsoil010 <- totsoilC2 %>%
   filter(bottomdepth_cm == 10, topdepth_cm == 0)
 
-#check ANOVA assumptions
 
+
+###
 #run ANOVAs
 fit <- aov(pool_value ~ veg, data = orgsoil010)
 summary(fit)
@@ -54,4 +55,115 @@ fit6 <- aov(pool_value ~ veg, data = BGBC2)
 summary(fit6)
 #p < 2e-16
 
-#run Tukey's
+
+###
+#check ANOVA assumptions
+plot(fit)
+plot(fit2)
+plot(fit3)
+plot(fit4)
+plot(fit5)
+plot(fit6)
+
+#test for normality
+test.lm = lm(pool_value ~ veg, data = orgsoil010)
+shapiro.test(residuals(test.lm))
+#p = 2.588e-12; so not normal
+
+test.lm2 = lm(pool_value ~ veg, data = orgsoil1020)
+shapiro.test(residuals(test.lm2))
+#p = 2.59e-10; so not normal
+
+test.lm3 = lm(pool_value ~ veg, data = totsoil010)
+shapiro.test(residuals(test.lm3))
+#p = 1.74e-12; so not normal
+
+test.lm4 = lm(pool_value ~ veg, data = litterC2)
+shapiro.test(residuals(test.lm4))
+#p = 5.512e-13; so not normal
+
+test.lm5 = lm(pool_value ~ veg, data = AGBC2)
+shapiro.test(residuals(test.lm5))
+#p < 2.2e-16; so not normal
+
+test.lm6 = lm(pool_value ~ veg, data = BGBC2)
+shapiro.test(residuals(test.lm6))
+#p = 6.515e-08; so not normal
+
+
+###
+#try to transform
+test.lm1a <- lm(log(pool_value) ~ veg, data = orgsoil010)
+shapiro.test(residuals(test.lm1a))
+#still not normal
+
+test.lm1b <- lm(sqrt(pool_value) ~ veg, data = orgsoil010)
+shapiro.test(residuals(test.lm1b))
+#still not normal
+
+test.lm2a <- lm(log(pool_value) ~ veg, data = orgsoil1020)
+shapiro.test(residuals(test.lm2a))
+#still not normal
+
+test.lm2b <- lm(sqrt(pool_value) ~ veg, data = orgsoil1020)
+shapiro.test(residuals(test.lm2b))
+#still not normal
+
+test.lm3a <- lm(log(pool_value) ~ veg, data = totsoil010)
+shapiro.test(residuals(test.lm3a))
+#normal
+
+###
+fit3b <- aov(log(pool_value) ~ veg, data = totsoil010)
+summary(fit3b)
+#p = 1.58e-09; compare to non-parametric below
+###
+
+test.lm3b <- lm(sqrt(pool_value) ~ veg, data = totsoil010)
+shapiro.test(residuals(test.lm3b))
+#still not normal
+
+test.lm4a <- lm(log(pool_value) ~ veg, data = litterC2)
+shapiro.test(residuals(test.lm4a))
+#still not normal
+
+test.lm4b <- lm(sqrt(pool_value) ~ veg, data = litterC2)
+shapiro.test(residuals(test.lm4b))
+#still not normal
+
+test.lm5a <- lm(log(pool_value) ~ veg, data = AGBC2)
+shapiro.test(residuals(test.lm5a))
+#still not normal
+
+test.lm5b <- lm(sqrt(pool_value) ~ veg, data = AGBC2)
+shapiro.test(residuals(test.lm5b))
+#still not normal
+
+test.lm6a <- lm(log(pool_value) ~ veg, data = BGBC2)
+shapiro.test(residuals(test.lm6a))
+#still not normal
+
+test.lm6b <- lm(sqrt(pool_value) ~ veg, data = BGBC2)
+shapiro.test(residuals(test.lm6b))
+#still not normal
+
+
+###
+#run non-parametrics
+kruskal.test(pool_value ~ veg, data = orgsoil010)
+#p=3.05e-09
+
+kruskal.test(pool_value ~ veg, data = orgsoil1020)
+#p=0.0619
+
+kruskal.test(pool_value ~ veg, data = totsoil010)
+#p=4.29e-08
+
+kruskal.test(pool_value ~ veg, data = litterC2)
+#p=2.46e-14
+
+kruskal.test(pool_value ~ veg, data = AGBC2)
+#p=5.119e-07
+
+kruskal.test(pool_value ~ veg, data = BGBC2)
+#p<2.2e-16
