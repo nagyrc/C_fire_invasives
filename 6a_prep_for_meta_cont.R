63f37ac3aef07ecb12908293e91d8992a07c169e
Chelsea Nagy <rana7082@CIRES-EL-LM-042.local>
2020-04-17 09:23:58 -0600

    not working

--- 6a_prep_for_meta_cont.R
+++ 6a_prep_for_meta_cont.R
@@ -66,10 +66,8 @@ unique(test3$veg) #1
 ###############
 cct <- siwf4 %>%
   group_by(study) %>%
-  summarise(numveg = n_distinct(veg,  na.rm = TRUE)) 
-rm(cct)
-#%>%
-  #filter(numveg > 1) 
+  summarise(numveg = n_distinct(veg,  na.rm = TRUE)) %>%
+  filter(numveg > 1) 
 cct$geometry <- NULL
 
 studies <- cct$study
